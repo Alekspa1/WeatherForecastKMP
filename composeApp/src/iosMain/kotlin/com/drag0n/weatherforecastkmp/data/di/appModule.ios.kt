9a52ca0c86fc512aeac27030bd0e9663e82f4560
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-02-18 15:34:37 +0300

    Update appModule.ios.kt

--- composeApp/src/iosMain/kotlin/com/drag0n/weatherforecastkmp/data/di/appModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/drag0n/weatherforecastkmp/data/di/appModule.ios.kt
@@ -4,3 +4,9 @@ import org.koin.core.module.Module
 
 actual val moduleLocation: Module
     get() = TODO("Not yet implemented")
+
+   // actual val moduleLocation = module {
+    //single { 
+    //    LocationIosImpl(get(named("IP_LOCATION"))) 
+   // } binds arrayOf(LocationRepository::class, PermissionRepository::class)
+//}
